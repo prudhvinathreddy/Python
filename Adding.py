f388a8df45fec4d1ef6c3168c2479f73f2c293ca
Prudhvi <145875841+prudhvinathreddy@users.noreply.github.com>
2023-12-09 03:50:47 -0500

    Create Adding.py
    Adding two numbers

---
+++ Adding.py
@@ -0,0 +1,12 @@
+#how to add two numbers in python
+print(2+5)
+
+#using variables
+a=2
+b=3
+print(a+b)
+
+#using functions
+def add_fun(x,y):
+    return x+y
+print("sum of two numbers is ", add_fun(2,3))
