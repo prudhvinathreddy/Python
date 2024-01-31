c02b4542c4f0c1092f08dfbe4aee700907c8a4fb
Prudhvi <145875841+prudhvinathreddy@users.noreply.github.com>
2024-01-31 06:42:30 +0000

    modified code for better execution

--- Projects/Beginners/Guessing.py
+++ Projects/Beginners/Guessing.py
@@ -1,23 +1,30 @@
 import random
+import math
+
+print("================================\n| WELCOME TO THE GUESSING GAME |\n================================")
+print()
 min_range = int(input('Enter the min range value - '))
 max_range = int(input('Enter max range value - '))
 guessing_number = random.randint(min_range,max_range)
+print("==============================================\n| I have a number in my mind - START GUESSING |\n==============================================")
+#print("")
 count = 0
-chances = round(math.log(upper-lower+1,2))
-print("\n You have only ", chances,  "chances to guess the number..!\n")
+chances = round(math.log(max_range-min_range+1,2))
+print("\nYou have only ", chances,  "chances to guess the number..!")
 
 while count<chances:
   count+=1
-  guess = int(input("Guess a number - "))
+  guess = int(input("\nGuess a number - "))
 
   if guessing_number == guess:
     print(" Woah...! You Did It")
+    break
   elif guessing_number > guess:
-    print("You guessed value is too small..! \n TRY AGAIN..! \n You only have ", 
+    print("You guessed value is too small..! \nTRY AGAIN..!\nYou only have ", 
           chances-count , "chances")
   elif guessing_number < guess:
-    print("You guessed value is too high..! \n TRY AGAIN..! \n You only have ", 
-          chances-count , "chances")
-if count>=chances:
-  print("\n The number is %d" % guessing_number)
-  print(""\t BETTER LUCK NEXT TIME!")
+    print("You guessed value is too high..! \nTRY AGAIN..!\nYou only have ", chances-count , "chances")
+    
+if count>chances:
+  print("\nThe number is ",guessing_number)
+  print("\tBETTER LUCK NEXT TIME!")
